a2a57268445270c46831f64a5c932362b1783879
hayoung <kanghayoung527@gmail.com>
2025-06-29 01:00:44 +0900

    solved 2588

---
+++ 2588_곱셈.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int third, forth, fifth, sixth;
+    third = a * (b % 10);
+    forth = a * (b / 10 % 10);
+    fifth = a * (b / 100);
+    sixth = third + (forth * 10) + (fifth * 100);
+
+    cout << third << endl;
+    cout << forth << endl;
+    cout << fifth << endl;
+    cout << sixth << endl;
+
+    return 0;
+}
